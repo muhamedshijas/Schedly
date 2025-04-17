27366d121665a5ee58bfb46b2306ef3d5f76087f
muhamedshijas <shijushijas157@gmail.com>
2025-04-17 21:08:38 +0530

    meaning display issue fixed

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -131,7 +131,7 @@ export default function Timetable() {
           boxShadow: 4,
           p: 2,
           maxWidth: 400,
-          maxHeight: 150,
+          maxHeight: 170,
           mx: "auto",
           mb: 4,
         }}
